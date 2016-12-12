dab35b2a2cf3ae20e330f24c434c59af55ddcb14
Brenden Prieto <bprieto@berkeley.edu>
2016-12-11 22:02:49 -0800

    added testing file for jasper

---
+++ Testing.py
@@ -0,0 +1,14 @@
+import re
+
+"""
+A nice file for testing Jasper's abilities
+"""
+
+WORDS = ["Testing"]
+
+def handle(text, mic, profile):
+	print("How can I help you?")
+
+def isValid(text):
+	return bool(re.search("Testing", text, re.IGNORECASE))
+
