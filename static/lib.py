d2ba183b4beec8a6295b2acf5b53693c9ff0d0fa
Brenden <bprieto@berkeley.edu>
2016-12-14 06:55:41 +0000

    added songs

--- static/lib.py
+++ static/lib.py
@@ -9,44 +9,122 @@ THREE_DURATIONS = ["quarter-note","quarter-note","quarter-note"]
 
 
 
-DAY_TRIPPER_NOTES = ["E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
-					 "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
-					 "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
-					 "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
-					 "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
-					 "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
-					 "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
-					 "A0","A3", "A4", "D2", "G2", "G0", "D2", "G4", "D2", "G0", "G2",
-					 "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
-					 "E2+A4", "E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4",
-					 "E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4",
-					 "E7+A7", "E7+A7","E7+A7","E7+A7","E2+A6","E2+A6","E2+A6","E2+A6",
-					 "A4+D6","A4+D6","A4+D6","A4+D6","A2+D4","A2+D4","A2+D4","A2+D4",
-					 "A2+D4+G4","A5","A6","D4","G4","G2","D4","G6","D4","G2","G4",
-					 "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2"]
+DAY_TRIPPER_FRETS = ["E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
+		     "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
+		     "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
+		     "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
+		     "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
+		     "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
+		     "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
+		     "A0","A3", "A4", "D2", "G2", "G0", "D2", "G4", "D2", "G0", "G2",
+		     "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2",
+		     "E2+A4", "E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4",
+	             "E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4","E2+A4",
+		     "E7+A7", "E7+A7","E7+A7","E7+A7","E2+A6","E2+A6","E2+A6","E2+A6",
+	             "A4+D6","A4+D6","A4+D6","A4+D6","A2+D4","A2+D4","A2+D4","A2+D4",
+		     "A2+D4+G4","A5","A6","D4","G4","G2","D4","G6","D4","G2","G4",
+		     "E0","E3", "E4", "A2", "D2", "D0", "A2", "D4", "A2", "D0", "D2"]
+DAY_TRIPPER_DURATION = ["quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note",
+						"quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note",
+						"quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note",
+						"quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note",
+						"quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note",
+						"quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note",
+						"quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note",
+						"quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note",
+						"quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note",
+						"eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note",
+						"eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note",
+						"eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note", "eighth-note","eighth-note","eighth-note",
+						"eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note",
+						"quarter-note","eighth-note","eighth-note", "eighth-note","eighth-note","quarter-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note"]
 
+LANDSLIDE_FRETS = ["A3","G0","D2","B1","A3","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A0","G0","D2","B1","A0","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A3","G0","D2","B1","A3","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A0","G0","D2","B1","A0","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A3","G0","D2","B1","A3","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A0","G0","D2","B1","A0","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A3","G0","D2","B1","A3","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A0","G0","D2","B1","A0","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A3","G0","D2","B1","A3","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A0","G0","D2","B1","A0","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A3","G0","D2","B1","A3","G0","D2","A2","G0","D0","B3","A2","G0","D0",
+				   "A0","G0","D2","B1","A0","D2","A0+B0","B1","D2","A2","D0","A3+B1","G0",
+				   "D2","e0","A3","D2","A2+B3","G0","D0","e3","A2","D0","A0","B1","G0","D2",
+				   "e2","A0","D2","A2+B3","G0","D0","e3","A2","D0"]
 
-
+LANDSLIDE_DURATIONS = ["eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","quarter-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","quarter-note","eighth-note","eighth-note",
+					   "eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note","eighth-note"]
 # "Let it Be" by Beatles (ukulele vesion)
-BEATLES_NOTES = ["A3+E0","G0","A3+E0","G0","C2+E3+A2","G0","C2+E3+A2","G0",
-				 "E0+A0","G2","E0+A0","G2","E1+A0","G2","E1+A0","G2","A3+E0",
-				 "G0","A3+E0","G0","C2+E3+A2","G0","C2+E3+A2","G0","C5+E5",
-				 "C4+E3","C2+E1","C0+E0","G0+C0+E0+A3","G0+C0+E0+A3","G0+C2+E3+A2",
-				 "G0+C0+E0+A3","G1+C0+E0+A0","G1+C0+E1+A0","G0+C0+E0+A3"]
+# BEATLES_NOTES = ["A3+E0","G0","A3+E0","G0","C2+E3+A2","G0","C2+E3+A2","G0",
+# 				 "E0+A0","G2","E0+A0","G2","E1+A0","G2","E1+A0","G2","A3+E0",
+# 				 "G0","A3+E0","G0","C2+E3+A2","G0","C2+E3+A2","G0","C5+E5",
+# 				 "C4+E3","C2+E1","C0+E0","G0+C0+E0+A3","G0+C0+E0+A3","G0+C2+E3+A2",
+# 				 "G0+C0+E0+A3","G1+C0+E0+A0","G1+C0+E1+A0","G0+C0+E0+A3"]
 
-# "Stairway to Heaven" by Led Zepplin (ukulele version)
+# # "Stairway to Heaven" by Led Zepplin (ukulele version)
 
-STAIRWAY_NOTES = ["G2","C0","E0","A0","G1+A2","E0","C0","A2","G0+A3",
-				  "E0","C0","A3","G2+E2","C2","E2","A0","G2","C0","E1",
-				  "A0","E1","C0","G2","G0+C2+E3+A2","G2+C0+E0+A0",
-				  "G2+C0+E0+A0","A0","A7", "G2","C0","E0","A0",
-				  "G1+A2","E0","C0","A2","G0+A3","E0","C2","A3","G2+E2",
-				  "C2","E2","A3","G2","C0","E1","A0","E1","C0","G2",
-				  "G0+C2+E3+A2","G2+C0+E0+A0","G2+C0+E0+A0", "A0","A2","A3",
-				  "C0","E0","A3","C2+A0","E2","C2","A0","C0+A0","E1",
-				  "C0","A0","G2+A2h", "A3","E0","A0","A2","A3","E0","C0",
-				  "A3","G0+A2","E3","C2","A2","C2+E2+A2p","A0","C2+E2+A0",
-				  "C2+E2+A0"]
-# "Brown Eyed Girl" by Van Morrison (ukulele version)
+# STAIRWAY_NOTES = ["G2","C0","E0","A0","G1+A2","E0","C0","A2","G0+A3",
+# 				  "E0","C0","A3","G2+E2","C2","E2","A0","G2","C0","E1",
+# 				  "A0","E1","C0","G2","G0+C2+E3+A2","G2+C0+E0+A0",
+# 				  "G2+C0+E0+A0","A0","A7", "G2","C0","E0","A0",
+# 				  "G1+A2","E0","C0","A2","G0+A3","E0","C2","A3","G2+E2",
+# 				  "C2","E2","A3","G2","C0","E1","A0","E1","C0","G2",
+# 				  "G0+C2+E3+A2","G2+C0+E0+A0","G2+C0+E0+A0", "A0","A2","A3",
+# 				  "C0","E0","A3","C2+A0","E2","C2","A0","C0+A0","E1",
+# 				  "C0","A0","G2+A2h", "A3","E0","A0","A2","A3","E0","C0",
+# 				  "A3","G0+A2","E3","C2","A2","C2+E2+A2p","A0","C2+E2+A0",
+# 				  "C2+E2+A0"]
+# # "Brown Eyed Girl" by Van Morrison (ukulele version)
 
-BROWN_NOTES = ["G0+A2","G2+A3","G4+A5"]
+# BROWN_NOTES = ["G0+A2","G2+A3","G4+A5"]
+
+
+#Guitar
+BLAKBIRD_NOTES = ["E3+B0", "G0", "A0+B1", "G0", "A2+B3", "G0", "A10+B12", "G0", "B12", "A10", "B12", "G0", "A10+B12", "G0", "B12", "A10", "B12", "G0",
+# verse
+    "E3+B0", "G0", "A0+B1", "A2+B3", "G0", "A10+B12", "G0", "B12", "A10", "B12", "G0", "A10+B12", "G0", "B12", "A10", "B12", "G0",
+    "A3+B5", "G0", "A4=B8", "G0", "A5+B7", "G0", "A6+B10", "G0", "A7+B8", "G0", "B8", "A7", "B8", "G0", "A6+B8", "G0", "B8", "A6"," B8", "G0",
+    "A5+B7", "G0", "A4+B8", "G0", "A3+B5", "G0", "B5", "A3", "B5", "G0", "A3+B4", "G0", "B4", "A3", "B4", "G0",
+    "A2+B3", "G0", "B3", "A2", "B3", "G0", "A0+B2", "G0", "B2", "A0", "B2", "G0", "D0+B1", "G0", "B1", "D0", "B1", "G0", "E3+B0", "G0","B0", "E3", "B0", "G0",
+# Chorus
+    "A8+B10", "G0", "A7+B8", "G0", "A5+B6", "G0", "A3+B5", "G0", "A1+B3", "G0", "B3", "A1", "B3", "G0", "A3+B5", "G0", "B5", "A3", "B5", "G0",
+    "A8+B10", "G0", "A7+B8", "G0", "A5+B6", "G0", "A3+B5", "G0", "A1+B3", "G0", "B3", "A1", "B3", "G0", "A0+B2", "G0", "B2", "A0", "B2", "G0",
+    "D0+B1", "G0", "B1", "D0", "B1", "G0", "B1",
+]
+
+
+
+ #hotel california
+#tempo: qua =73
+ 
+hotel_frets = ["E0+A2+D2+G0+B0", "A2", "G0", "D2", "B0", "D2", "G0", "e0", "G0", "G2+B0", "D1", "G2", "B0", "A2", "e2", "G2", "B0", "D1", "D0", "G2", "e5", "B3", "e0", "B3", "D0", "G2", "B3", "A0", "G0", "B0", "A4", "e0", "G0", "D2", "B0", "G0", "G0", "e0", "A3", "e0", "G0", "e0","B1", "    D2", "G0", "e0", "A3", "B1", "D0", "D2", "G0", "D0", "G0", "B0", "G0", "e3", "D0", "B0", "A2", "G0", "B0", "E3", "D0", "B0", "A0", "e0", "G0", "D2", "B1", "D2", "B1", "e0", "D2", "G0", "E0", "D2", "B0", "A2", "D1", "G2", "G2", "B0", "D1", "G2", "e2"  ] 
+
+measure_1 = [8, 16, 16, 16, 16, 16, 16, 4, 8, 8]
+measure_2 = [8, 16, 16, 16, 16, 16, 16, 2]
+measure_3 = [8, 16, 16, 16, 8, 16, 8, 16, 16, 8, 16, 16]
+measure_4 = [8, 16, 16, 16, 16, 16, 16, 2]
+measure_5 = [8, 16, 16, 16, 16, 16, 16, 8 ,16, 16, 16, 16, 8]
+measure_6 = [8, 16, 16, 16, 16, 16, 16, 8 ,16 ,16, 8, 16, 16]
+measure_7 = [8, 16, 16, 16, 16 ,16 ,16, 8, 16, 16, 8, 16, 16]
+measure_8 = [8, 16, 16, 16, 16, 16, 16, 2]
+ 
+hotel_note_lengths = measure_1 + measure_2 + measure_3 + measure_4 + measure_5 + measure_6 + measure_7 + measure_8
+HOTEL_LENGTH = ['eighth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'quarter-note', 'eighth-note', 'eighth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note'    , 'sixteenth-note', 'half-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note',     'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'half-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-    note', 'eighth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteen    th-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'eighth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note', 'sixteenth-note',     'sixteenth-note', 'half-note']
+ 
+ 
+ 
+ 
